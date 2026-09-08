8491fc0fd4761f91ea01f6a07d3150d385ba426f
rizkijanuari <rizkijanuar.work@gmail.com>
2026-09-08 22:11:34 +0700

    FEAT[.]: .

--- src/screens/screen10/index.js
+++ src/screens/screen10/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Screen10() {
+export default function Screen10({ route }) {
+  const { fromScreen6, fromScreen7, fromScreen8, fromScreen9 } = route.params;
   return (
     <SafeAreaView
       style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -22,6 +23,44 @@ export default function Screen10() {
           }}>
           <Text style={{ color: 'white' }}>done!!!</Text>
         </TouchableOpacity>
+        <View>
+          <Text
+            style={{
+              backgroundColor: 'red',
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              color: 'white',
+            }}>
+            {fromScreen6}
+          </Text>
+          <Text
+            style={{
+              backgroundColor: 'red',
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              color: 'white',
+            }}>
+            {fromScreen7}
+          </Text>
+          <Text
+            style={{
+              backgroundColor: 'red',
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              color: 'white',
+            }}>
+            {fromScreen8}
+          </Text>
+          <Text
+            style={{
+              backgroundColor: 'red',
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              color: 'white',
+            }}>
+            {fromScreen9}
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
