cc76dc34ce37e25122dcad711fe64c8b46b08be2
rizkijanuari <rizkijanuar.work@gmail.com>
2026-09-08 22:05:30 +0700

    FEAT[.]: .

--- src/screens/screen8/index.js
+++ src/screens/screen8/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Screen8() {
+export default function Screen8({ navigation, route }) {
+  const { fromScreen6, fromScreen7 } = route.params;
+  const handleNavScreen8 = () => {
+    navigation.navigate('Screen9', {
+      fromScreen6: fromScreen6,
+      fromScreen7: fromScreen7,
+      fromScreen8: 'gue dari screen 8 bro!',
+    });
+  };
+
   return (
     <SafeAreaView
       style={{
@@ -17,6 +26,7 @@ export default function Screen8() {
         }}>
         <Text>screen8!</Text>
         <TouchableOpacity
+          onPress={handleNavScreen8}
           style={{
             paddingVertical: 12,
             paddingHorizontal: 12,
@@ -31,6 +41,26 @@ export default function Screen8() {
             goto Screen9!
           </Text>
         </TouchableOpacity>
+        <View>
+          <Text
+            style={{
+              backgroundColor: 'red',
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              color: 'white',
+            }}>
+            {fromScreen6}
+          </Text>
+          <Text
+            style={{
+              backgroundColor: 'red',
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              color: 'white',
+            }}>
+            {fromScreen7}
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
