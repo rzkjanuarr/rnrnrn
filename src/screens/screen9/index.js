6406237c0e2ac596492f40c0f4625e8abd2946cc
rizkijanuari <rizkijanuar.work@gmail.com>
2026-09-08 22:09:00 +0700

    FEAT[.]: .

--- src/screens/screen9/index.js
+++ src/screens/screen9/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Screen9() {
+export default function Screen9({ navigation, route }) {
+  const { fromScreen6, fromScreen7, fromScreen8 } = route.params;
+  const handleNavScreen9 = () => {
+    navigation.navigate('Screen10', {
+      fromScreen6: fromScreen6,
+      fromScreen8: fromScreen8,
+      fromScreen7: fromScreen7,
+      fromScreen9: 'gue dari screen 9!',
+    });
+  };
+
   return (
     <SafeAreaView
       style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -13,6 +23,7 @@ export default function Screen9() {
         }}>
         <Text>screen9!</Text>
         <TouchableOpacity
+          onPress={handleNavScreen9}
           style={{
             justifyContent: 'center',
             alignItems: 'center',
@@ -27,6 +38,35 @@ export default function Screen9() {
             goto screen10!
           </Text>
         </TouchableOpacity>
+        <View>
+          <Text
+            style={{
+              backgroundColor: 'red',
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              color: 'white',
+            }}>
+            {fromScreen6}
+          </Text>
+          <Text
+            style={{
+              backgroundColor: 'red',
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              color: 'white',
+            }}>
+            {fromScreen7}
+          </Text>
+          <Text
+            style={{
+              backgroundColor: 'red',
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              color: 'white',
+            }}>
+            {fromScreen8}
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
