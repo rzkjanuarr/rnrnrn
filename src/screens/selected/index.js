d8c0dea2f49448f3d607dd6fd8ba659f50558c71
rizkijanuari <rizkijanuar.work@gmail.com>
2026-09-08 14:20:40 +0700

    FEAT[.]: .

--- src/screens/selected/index.js
+++ src/screens/selected/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { Text, TouchableOpacity, View, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ChevronRight } from 'lucide-react-native';
 
 export default function SelectedScreen() {
   const [selected, setSelected] = useState('expanses');
   const [pilih, setPilih] = useState('arab');
+  const [g, setG] = useState('surya');
+  const [chose, setChose] = useState('Urban Hoddie');
 
   const KategoriFinance = () => {
     return (
@@ -87,6 +90,7 @@ export default function SelectedScreen() {
       name: 'latte',
     },
   ];
+
   const KategoriKopi = ({ item }) => {
     return (
       <View
@@ -118,6 +122,152 @@ export default function SelectedScreen() {
     );
   };
 
+  const menuDataRokok = [
+    {
+      id: '1',
+      name: 'surya',
+    },
+    {
+      id: '2',
+      name: 'essei',
+    },
+    {
+      id: '3',
+      name: 'juara filter',
+    },
+    {
+      id: '4',
+      name: 'juara kretek',
+    },
+    {
+      id: '5',
+      name: 'camel',
+    },
+    {
+      id: '6',
+      name: 'malboro',
+    },
+  ];
+
+  const KategoriRokok = ({ item }) => {
+    return (
+      <View
+        style={{
+          paddingTop: 24,
+          marginLeft: 24,
+        }}>
+        <TouchableOpacity
+          onPress={() => setG(item.name)}
+          style={{
+            backgroundColor: 'transparent',
+            borderWidth: 1,
+            borderColor: g === item.name ? 'red' : 'grey',
+            paddingVertical: 12,
+            width: 80,
+            paddingHorizontal: 12,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
+            numberOfLines={1}
+            ellipsizeMode="tail"
+            style={{
+              fontSize: 12,
+              color: g === item.name ? 'red' : 'grey',
+              fontWeight: 'bold',
+            }}>
+            {item.name}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  const dataHodie = [
+    {
+      id: '1',
+      name: 'Urban Hoddie',
+      price: '$99',
+    },
+    {
+      id: '2',
+      name: 'Kanban Hoddie',
+      price: '$15',
+    },
+    {
+      id: '3',
+      name: 'Bodie Hoddie',
+      price: '$10',
+    },
+    {
+      id: '4',
+      name: 'Dono Hoddie',
+      price: '$10',
+    },
+    {
+      id: '5',
+      name: 'Doni Hoddie',
+      price: '$10',
+    },
+  ];
+
+  const KategoriHodie = ({ item }) => {
+    return (
+      <View
+        style={{
+          marginLeft: 24,
+          marginTop: 24,
+        }}>
+        <TouchableOpacity
+          onPress={() => setChose(item.name)}
+          style={{
+            paddingVertical: 24,
+            borderWidth: 1,
+            backgroundColor: chose === item.name ? '#484948' : 'grey',
+            borderRadius: 999,
+            width: 200,
+            flexDirection: 'row',
+            padding: 20,
+          }}>
+          <View
+            style={{
+              flex: 1,
+            }}>
+            <Text
+              numberOfLines={1}
+              ellipsizeMode="tail"
+              style={{
+                color: 'white',
+                fontSize: 16,
+                fontWeight: 'bold',
+              }}>
+              {item.name}
+            </Text>
+            <Text
+              numberOfLines={1}
+              ellipsizeMode="tail"
+              style={{
+                color: 'white',
+                marginTop: 2,
+                fontSize: 12,
+                fontWeight: 'normal',
+              }}>
+              {item.price}
+            </Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: chose == item.name ? '#bcff5f' : 'grey',
+              padding: 8,
+              borderRadius: 99,
+            }}>
+            <ChevronRight size="24" color="black" />
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView>
       <KategoriFinance />
@@ -127,6 +277,22 @@ export default function SelectedScreen() {
         data={menuData}
         keyExtractor={(item) => item.id.toString()}
         renderItem={KategoriKopi}
+      />
+      {/* <KategoriRokok /> */}
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        data={menuDataRokok}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={KategoriRokok}
+      />
+      {/* <KategoriHodie /> */}
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        data={dataHodie}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={KategoriHodie}
       />
     </SafeAreaView>
   );
